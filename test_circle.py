9ce44b3531651fa6b07560753b34d113565e9a4d
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2024-03-12 15:50:50 +0700

    test some test for circle

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,31 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+import math
+
 
 # TODO write 3 tests as described above
+class TestCircle(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_add_typical_value(self):
+        circle1 = Circle(3)
+        circle2 = Circle(4)
+        new_circle = circle1.add_area(circle2)
+        self.assertEqual(5, new_circle.get_radius())
+        self.assertEqual(math.pi * 5 * 5, new_circle.get_area())
+
+    def test_add_only_one_value(self):
+        circle1 = Circle(3)
+        circle2 = Circle(0)
+        new_circle = circle1.add_area(circle2)
+        self.assertEqual(3, new_circle.get_radius())
+        self.assertEqual(math.pi * 3 * 3, new_circle.get_area())
+
+    def test_circle_have_negative_radius(self):
+        circle1 = Circle(-1)
+        with self.assertRaises(Exception):
+            circle1.get_radius()
